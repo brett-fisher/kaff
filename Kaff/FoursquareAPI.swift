47a60b36107eb7eb69270ffa7a8ad29a3eb6901b
Brett Fisher <bdf5106@psu.edu>
2018-06-28 21:30:56 -0400

    First commit of the project.

---
+++ Kaff/FoursquareAPI.swift
@@ -0,0 +1,14 @@
+//
+//  FoursquareAPI.swift
+//  Kaff
+//
+//  Created by Brett Fisher on 6/28/18.
+//  Copyright © 2018 MooseFace, LLC. All rights reserved.
+//
+
+import Foundation
+
+class FoursquareAPI: NSObject {
+  
+  
+}
